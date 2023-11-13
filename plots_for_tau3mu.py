a1d4082c1f0a9b9cfe38c6de66197cd8eda4da9e
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-11-13 17:19:33 +0100

    Update plots_for_tau3mu.py

--- plots_for_tau3mu.py
+++ plots_for_tau3mu.py
@@ -231,8 +231,7 @@ def bdt_taumass_plot(config, categories, year):
     n_signal = len(bdt_range_signal)
     n_bkg = len(bdt_range_bkg)
 
-    #for i in range(3):
-    for i in range(1):
+    for i in range(3):
         hTrain_signal = []
         hTrain_bkg = []
 
