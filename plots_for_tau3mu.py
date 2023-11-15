aa0e52e106e917a3ce17a1f5ad47df12cc150050
Marco <127106076+Ma128-bit@users.noreply.github.com>
2023-11-15 14:24:13 +0100

    Update plots_for_tau3mu.py

--- plots_for_tau3mu.py
+++ plots_for_tau3mu.py
@@ -31,7 +31,7 @@ def bdt_KS_plot(config, fold_index, categories, year):
 
     if date.startswith("/"):
         date = date[1:]
-
+        
     if output_path.startswith("/"):
         output_path = output_path[1:]
     
